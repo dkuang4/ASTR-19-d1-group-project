61469fc91d22316b1a9f2459148c57e9ebc6e451
srpradha <srpradha@ucsc.edu>
2023-11-30 20:49:37 -0800

    got the right data

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import scipy
 import pandas as pd
 import numpy as np
-import datetime
+import matplotlib.pyplot as plt
 
 # read in data from ASTR19_S22_group_project_data.txt
 # define an oscillatory function in Python to describe intra- and inter-day variations in the tide
@@ -26,18 +26,41 @@ def read_and_clean_data():
     df["minutes"] = df["datetime"].dt.hour * 60 + df["datetime"].dt.minute
     df["time"] = ((df["day"] - 1) * 24 * 60) + df["minutes"]
 
-    df.drop(columns=["day", "24_hr", "datetime", "minutes"], inplace=True)
-    df = df[["time", "tide_height"]]
+    df.drop(columns=["day", "24_hr", "datetime"], inplace=True)  #  "minutes"
+    df = df[["minutes", "tide_height"]]
 
     df.to_csv("cleaned_data.csv", index=False)
-
-
-df = pd.read_csv("cleaned_data.csv")
-
-print(df)
 
 
 # formula from TA
 # A * sin(B * t + C) + D
 def oscillatory_func(t, A, B, C, D):
-    return A * np.sin(B * t + C) + D
+    return A * np.sin(t + C) + B * np.sin(30 * t + D)
+
+
+def fit_and_plot(df, x_axis, y_axis="tide_height"):
+    params, covariance = scipy.optimize.curve_fit(
+        oscillatory_func, df[x_axis], df[y_axis], sigma=np.full(len(df), 0.25)
+    )
+
+    # plotting
+    A, B, C, D = params
+
+    fitted_curve = oscillatory_func(df[x_axis], A, B, C, D)
+
+    plt.scatter(df[x_axis], df[y_axis], label="Original Data")
+    plt.plot(df[x_axis], fitted_curve, "r-", label="Fitted Curve")
+    plt.xlabel("Time (minutes)")
+    plt.ylabel("Height")
+    plt.legend()
+    plt.show()
+
+
+def main():
+    # read_and_clean_data()
+    df = pd.read_csv("cleaned_data.csv")
+    fit_and_plot(df, "minutes", "tide_height")
+
+
+if __name__ == "__main__":
+    main()
